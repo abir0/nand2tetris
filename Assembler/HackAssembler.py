d8fe557cc3c6b7c211d385684fcb338b4d7adcae
Abir Hasan <abir.spsc@gmail.com>
2021-01-16 00:27:06 +0600

    Minor formatting of dictionaries

--- Assembler/HackAssembler.py
+++ Assembler/HackAssembler.py
@@ -22,13 +22,13 @@ def table_map(asm):
     """Maps the variable names to register addresses."""
     # tableionary of mappings between pre-defined names
     table = {
-        "SP": 0,
-        "LCL": 1,
-        "ARG": 2,
-        "THIS": 3,
-        "THAT": 4,
+        "SP"    : 0,
+        "LCL"   : 1,
+        "ARG"   : 2,
+        "THIS"  : 3,
+        "THAT"  : 4,
         "SCREEN": 16384,
-        "KBD": 24576,
+        "KBD"   : 24576,
         }
 
     for i in range(0, 16):
@@ -112,18 +112,19 @@ def parser(line, table, comp_dict, dest_dict, jump_dict):
 def main(args):
     with open(args[0]) as asm_file:
         data = asm_file.readlines()
+
     # Three dictoniaries store machine translations for
     # each parts of the C instruction
     comp_dict = {
-        "0": "0101010",
-        "1": "0111111",
-        "-1": "0111010",
-        "D": "0001100",
-        "A": "0110000",
-        "!D": "0001101",
-        "!A": "0110001",
-        "-D": "0001111",
-        "-A": "0110011",
+        "0"  : "0101010",
+        "1"  : "0111111",
+        "-1" : "0111010",
+        "D"  : "0001100",
+        "A"  : "0110000",
+        "!D" : "0001101",
+        "!A" : "0110001",
+        "-D" : "0001111",
+        "-A" : "0110011",
         "D+1": "0011111",
         "A+1": "0110111",
         "D-1": "0001110",
@@ -133,9 +134,9 @@ def main(args):
         "A-D": "0000111",
         "D&A": "0000000",
         "D|A": "0010101",
-        "M": "1110000",
-        "!M": "1110001",
-        "-M": "1110011",
+        "M"  : "1110000",
+        "!M" : "1110001",
+        "-M" : "1110011",
         "M+1": "1110111",
         "M-1": "1110010",
         "D+M": "1000010",
@@ -147,24 +148,24 @@ def main(args):
 
     dest_dict = {
         "null": "000",
-        "M": "001",
-        "D": "010",
-        "A": "100",
-        "MD": "011",
-        "AM": "101",
-        "AD": "110",
-        "AMD": "111"
+        "M"   : "001",
+        "D"   : "010",
+        "A"   : "100",
+        "MD"  : "011",
+        "AM"  : "101",
+        "AD"  : "110",
+        "AMD" : "111"
         }
 
     jump_dict = {
         "null": "000",
-        "JGT": "001",
-        "JEQ": "010",
-        "JGE": "011",
-        "JLT": "100",
-        "JNE": "101",
-        "JLE": "110",
-        "JMP": "111"
+        "JGT" : "001",
+        "JEQ" : "010",
+        "JGE" : "011",
+        "JLT" : "100",
+        "JNE" : "101",
+        "JLE" : "110",
+        "JMP" : "111"
         }
 
     table = table_map(data)
