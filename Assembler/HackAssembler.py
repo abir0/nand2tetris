067e2efab5693bc5b16ef632d19032cf20feec1f
Abir Hasan <abir.spsc@gmail.com>
2021-01-19 14:32:31 +0600

    Change flags in parser function

--- Assembler/HackAssembler.py
+++ Assembler/HackAssembler.py
@@ -43,18 +43,17 @@ def map_table(asm):
     count = -1      # keep track of instruction memory position
 
     for line in asm:
-        parsed, flags =  parser(line)
-        A_instruction, A_numeric, C_instruction, goto_instruction = flags
+        parsed, flag =  parser(line)
 
-        if goto_instruction:
+        if flag == "GOTO_INSTRUCTION":
             table[parsed] = count + 1      # add next position after goto
-        elif A_numeric:
+        elif flag == "A_DECIMAL":
             count += 1
-        elif A_instruction:
+        elif flag == "A_INSTRUCTION":
             if parsed not in variables_list:
                 variables_list.append(parsed)    # append to list if it doesn't exist
             count += 1
-        elif C_instruction:
+        elif flag == "C_INSTRUCTION":
             count += 1
 
     for i in variables_list:
@@ -73,24 +72,18 @@ def parser(line):
     line = re.sub(r'//.*', '' , line)   # remove comment
     line = line.strip()                 # remove whitespace
 
-    # Flags
-    A_instruction = False
-    A_numeric = False
-    C_instruction = False
-    goto_instruction = False
-
     # Parse A instruction, return int or string
     if line.find('@') == 0:
         try:
             parsed = int(line[1:])
-            A_numeric = True
+            flag = "A_DECIMAL"
         except:
             parsed = line[1:]
-            A_instruction = True
+            flag = "A_INSTRUCTION"
 
     elif line.startswith("(") and line.endswith(")"):
         parsed = line[1:-1]
-        goto_instruction =True
+        flag = "GOTO_INSTRUCTION"
     else:
         # Parse C instruction, return tuple
         if line.find(';') != -1:
@@ -99,18 +92,15 @@ def parser(line):
             if comp.find('=') != -1:
                 dest, comp = comp.split('=')    # dest = comp ; jump
             parsed = comp, dest, jump
-            C_instruction = True
+            flag = "C_INSTRUCTION"
 
         elif line.find('=') != -1:
             dest, comp = line.split('=')        # dest = comp
             jump = "null"
             parsed = comp, dest, jump
-            C_instruction = True
-        else:
-            parsed = None
+            flag = "C_INSTRUCTION"
 
-    flags = A_instruction, A_numeric, C_instruction, goto_instruction
-    return parsed, flags
+    return parsed, flag
 
 
 def main(filename):
@@ -181,29 +171,26 @@ def main(filename):
     machine_code = []
 
     for line in data:
-        parsed, flags = parser(line)
-        A_instruction, A_numeric, C_instruction, goto_instruction = flags
+        parsed, flag = parser(line)
 
-        if A_instruction:
+        if flag == "A_INSTRUCTION":
             dec = table[parsed]
             bin = dec2bin(dec)
-        elif A_numeric:
+        elif flag == "A_DECIMAL":
             bin = dec2bin(parsed)
-        elif C_instruction:
+        elif flag == "C_INSTRUCTION":
             # Join parsed components from corresponding dictionaries
             # and pad with 1's to match 16 bits of A instruction
             bin = '111' + comp[parsed[0]] + dest[parsed[1]] + jump[parsed[2]]
         else:
             continue
-
+            
         machine_code.append(bin)
 
-    if filename.endswith(".asm"):
-        filename = filename.replace(".asm", ".hack")
-    else:
-        filename = filename + ".hack"
+    if not filename.endswith(".asm"):
+        filename = filename + ".asm"
 
-    with open(filename, "w") as outfile:
+    with open(filename.replace(".asm", ".hack"), "w") as outfile:
         outfile.write('\n'.join(machine_code)) # Join each list item with newline
 
 
