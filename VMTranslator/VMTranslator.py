d894d1de9e5f1481288821066186c7fec5b9cd50
Abir Hasan <abir.spsc@gmail.com>
2021-01-21 23:17:16 +0600

    Finalizing VMTranslator

--- VMTranslator/VMTranslator.py
+++ VMTranslator/VMTranslator.py
@@ -1,11 +1,38 @@
 import sys
 import re
+from CodeWriter import *
+from Parser import *
 
+def main(filename):
 
-def main():
-    # Work in progress
-    return
+    try:
+        P = Parser(filename)
+
+    except FileNotFoundError:
+        print("No such file: \'{}\'\nPlease enter correct filename".format(filename))
+        sys.exit(1)
+
+    while P.hasMoreCommads():
+        P.nextCommad()
+        if not P.isValidCommand():
+            print("Command is not valid.")
+            sys.exit(1)
+
+        C = CodeWriter(filename)
+        if P.commandType() == "C_ARITHMATIC":
+            C.writeComment(P.command)
+            C.writeArithmatic(P.firstArgument())
+        elif P.commandType() == "C_PUSH":
+            C.writeComment(P.command)
+            C.writePushPop("push", P.firstArgument(), P.secondArgument())
+        elif P.commandType() == "C_POP":
+            C.writeComment(P.command)
+            C.writePushPop("pop", P.firstArgument(), P.secondArgument())
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage: VMTranslator <filename>")
+        sys.exit(1)
+
+    main(sys.argv[1])
