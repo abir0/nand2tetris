e23fcea9975fbf45ed98c10b1f4379209545b5ea
Abir Hasan <abir.spsc@gmail.com>
2021-02-20 16:48:04 +0600

    Add staticmethods and update Tokenizer class

--- Compiler/Tokenizer.py
+++ Compiler/Tokenizer.py
@@ -15,37 +15,42 @@ class Tokenizer:
     def __init__(self, filename):
         self.filename = filename
 
-    def read_file(self):
-        """Read each file and get the data."""
+    @staticmethod
+    def read_file(filename):
+        """Read the file and get its contents."""
 
-        with open(self.filename, "r") as infile:
+        with open(filename, "r") as infile:
             file_data = str(infile.read())
-            self.file = Tokenizer.process_lines(file_data)
 
-    @ staticmethod
-    def process_lines(file_data):
-        """Return the list of words from the file data."""
+        return file_data
+
+    @staticmethod
+    def remove_comments(file_data):
+        """Return the list of words from the data."""
 
         comment_pattern1 = r"\/\/.*?(\r\n|\r|\n)"
         comment_pattern2 = r"\/\*.*?\*\/"
+
         file_data = re.sub(comment_pattern1, "", file_data)
         file_data = re.sub(comment_pattern2, "", file_data)
+
         return file_data
 
-    def tokenize(self):
-        """Generate tokens from the data."""
+    @staticmethod
+    def get_tokens(file_data):
+        """Return the tokens from the data."""
 
         tokens = []
         token = ""
         str_flag = False
-        for c in str(self.file):
+        for c in str(file_data):
             if c in Tokenizer.SYMBOLS:
                 tokens.append(token.strip())
                 tokens.append(c)
                 token = ""
             elif c in ["\""]:
                 str_flag = not str_flag
-                tokens.append(token)
+                tokens.append(c + tokenc + c)
                 token = ""
             elif c in [" "]:
                 if str_flag:
@@ -55,20 +60,47 @@ class Tokenizer:
                 token = ""
             else:
                 token += c
-        self.tokens = list(filter(len, tokens))
-        return self.tokens
+        tokens = list(filter(len, tokens))
+        return tokens
 
-    def write_file(self):
+    def tokenize(self):
+        """Generate xml labels from the tokens."""
+
+        integer_pattern = r"\b[0-9]{1,5}\b"
+        string_pattern = r"\".*?\""
+        identifier_pattern = r"\b[A-Za-z_][A-Za-z0-9_]*\b"
+
+        file_data = Tokenizer.read_file(self.filename)
+        file_data = Tokenizer.remove_comments(file_data)
+        tokens = Tokenizer.get_tokens(file_data)
+
+        for i, token in enumerate(tokens):
+            if token in Tokenizer.KEYWORDS:
+                tokens[i] = "<keyword> " + token + " </keyword>"
+            elif token in Tokenizer.SYMBOLS:
+                tokens[i] = "<symbol> " + token + " </symbol>"
+            elif re.search(identifier_pattern, token) is not None:
+                tokens[i] = "<identifier> " + token + " </identifier>"
+            elif re.search(integer_pattern, token) is not None:
+                tokens[i] = "<integerConstant> " + \
+                    token + " </integerConstant>"
+            elif re.search(string_pattern, token) is not None:
+                tokens[i] = "<stringConstant> " + token + " </stringConstant>"
+
+        return tokens
+
+    def write(self, tokens):
         """Write the tokens into each files."""
 
-        out_filename = self.filename.replace(".jack", ".txt")
+        out_filename = self.filename.replace(".jack", ".xml")
         with open(out_filename, "w") as outfile:
-            outfile.write("\n".join(self.tokens))
+            outfile.write("<tokens>\n")
+            outfile.write("\n".join(tokens))
+            outfile.write("\n</tokens>")
 
 
 if __name__ == "__main__":
 
     T = Tokenizer(sys.argv[1])
-    T.read_file()
-    T.tokenize()
-    T.write_file()
+    tokens = T.tokenize()
+    T.write(tokens)
