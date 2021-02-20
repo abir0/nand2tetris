8ae6e3883997da702169dfa4b57ecfc720014db5
Abir Hasan <abir.spsc@gmail.com>
2021-02-20 13:20:57 +0600

    Add parser module

---
+++ Compiler/Parser.py
@@ -0,0 +1,18 @@
+
+
+class CompilationEngine:
+
+    def compileStatements():
+        pass
+
+    def compileIfStatement():
+        pass
+
+    def compileWhileStatement():
+        pass
+
+    def compileLetStatement():
+        pass
+
+    def compileTerm():
+        pass
