72436556e566f68c4ff1618aca8f5e03e960ffa0
Abir Hasan <abir.spsc@gmail.com>
2021-03-11 01:30:11 +0600

    Add new module

---
+++ Compiler/IndentifyXML.py
@@ -0,0 +1,24 @@
+import sys
+import re
+
+def main(filename):
+
+    open_tag = r'\<[^\\]*?\>'
+    close_tag = r'\<\\.*?\>'
+    with open(filename, 'r') as fh:
+        data = fh.readlines()
+
+    with open(filename.replace(".xml", "P.xml"), 'r') as fh:
+        indent_level = 0
+        for line in data:
+            if re.search(close_tag, line) is not None:
+                indent_level -= 1
+            line = "\t" * indent_level + line
+            fh.writeline(line)
+            if re.search(open_tag, line) is not None:
+                indent_level += 1
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
