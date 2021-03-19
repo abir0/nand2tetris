3f0f1a93d899abbdaa0d53608788f4b288d5ddbb
Abir Hasan <abir.spsc@gmail.com>
2021-03-19 13:36:33 +0600

    flag name changed

--- Compiler/SymbolTable.py
+++ Compiler/SymbolTable.py
@@ -12,34 +12,34 @@ class SymbolTable:
         self.subroutine_table = dict()
         self.index_subroutine = dict()
 
-    def define(self, name, type, kind, kind_flag=False):
-        if kind_flag:
-            self.varCount(kind, class_flag=True)
+    def define(self, name, type, kind, class=False):
+        if class:
+            self.varCount(kind, class=True)
             self.subroutine_table[name] = (type, kind, self.index_class[kind])
         else:
-            self.varCount(kind, class_flag=False)
+            self.varCount(kind, class=False)
             self.subroutine_table[name] = (type, kind, self.index_subroutine[kind])
 
-    def varCount(self, kind, class_flag):
-        if class_flag:
+    def varCount(self, kind, class=False):
+        if class:
             self.index_class[kind] = self.index_class.get(kind, -1) + 1
         else:
             self.index_subroutine[kind] = self.index_subroutine.get(kind, -1) + 1
 
-    def TypeOf(self, name, class_flag):
-        if class_flag:
+    def TypeOf(self, name, class=False):
+        if class:
             self.class_table[name][0]
         else:
             self.subroutine_table[name][0]
 
-    def KindOf(self, name, class_flag):
-        if class_flag:
+    def KindOf(self, name, class=False):
+        if class:
             self.class_table[name][1]
         else:
             self.subroutine_table[name][1]
 
-    def IndexOf(self, name, class_flag):
-        if class_flag:
+    def IndexOf(self, name, class=False):
+        if class:
             self.class_table[name][2]
         else:
             self.subroutine_table[name][2]
