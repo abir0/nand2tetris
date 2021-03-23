81f5cfede46be6f4e9cd53a544aeae3fd200b912
Abir Hasan <abir.spsc@gmail.com>
2021-03-24 00:49:25 +0600

    Update SymbolTable

--- Compiler/SymbolTable.py
+++ Compiler/SymbolTable.py
@@ -14,19 +14,19 @@ class SymbolTable:
 
     def define(self, name, type, kind, class_flag=False):
         if class_flag:
-            index = self.varCount(kind, class_flag=True) + 1
+            index = self.varCount(kind, class_flag=True)
             self.index_class[kind] = index
             self.class_table[name] = (type, kind, index)
         else:
-            index = self.varCount(kind, class_flag=False) + 1
+            index = self.varCount(kind, class_flag=False)
             self.index_subroutine[kind] = index
             self.subroutine_table[name] = (type, kind, index)
 
     def varCount(self, kind, class_flag=False):
         if class_flag:
-            index = self.index_class.get(kind, -1)
+            index = self.index_class.get(kind, -1) + 1
         else:
-            index = self.index_subroutine.get(kind, -1)
+            index = self.index_subroutine.get(kind, -1) + 1
         return index
 
     def TypeOf(self, name):
@@ -43,7 +43,10 @@ class SymbolTable:
         try:
             kind = self.subroutine_table[name][1]
         except:
-            kind = self.class_table[name][1]
+            try:
+                kind = self.class_table[name][1]
+            except:
+                kind = None
         return kind
 
     def IndexOf(self, name):
