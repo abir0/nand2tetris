c0e3c3210d53db1499b2e79338cf8fb735abef4e
Abir Hasan <abir.spsc@gmail.com>
2021-03-23 12:50:23 +0600

    Fix: SymbolTable index dictionaries

--- Compiler/SymbolTable.py
+++ Compiler/SymbolTable.py
@@ -15,9 +15,11 @@ class SymbolTable:
     def define(self, name, type, kind, class_flag=False):
         if class_flag:
             index = self.varCount(kind, class_flag=True) + 1
+            self.index_class[kind] = index
             self.subroutine_table[name] = (type, kind, index)
         else:
             index = self.varCount(kind, class_flag=False) + 1
+            self.index_subroutine[kind] = index
             self.subroutine_table[name] = (type, kind, index)
 
     def varCount(self, kind, class_flag=False):
