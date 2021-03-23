cb3193e8f5ebe3df3cad231fb01067d1d7fac610
Abir Hasan <abir.spsc@gmail.com>
2021-03-23 13:38:31 +0600

    Issue fix: term, array, let statement

--- Compiler/CompilationEngine.py
+++ Compiler/CompilationEngine.py
@@ -27,6 +27,7 @@ class CompilationEngine:
         self.label = 0
         self.verbose = verbose
 
+
     def compileClass(self):
         self.Tokens.advance()
         if self.Tokens.keyWord() == "CLASS":
@@ -45,6 +46,7 @@ class CompilationEngine:
                         self.compileSubroutineDec(class_name)
                     if self.Tokens.symbol() == "}":
                         pass
+
 
     def compileClassVarDec(self):
         if self.Tokens.keyWord() in ["STATIC", "FIELD"]:
@@ -67,6 +69,7 @@ class CompilationEngine:
                         self.Tokens.advance()
                 if self.Tokens.symbol() == ";":
                     self.Tokens.advance()
+
 
     def compileSubroutineDec(self, class_name):
         nLocals = self.symbol_table.varCount("this", class_flag=True)
@@ -111,6 +114,7 @@ class CompilationEngine:
                                 self.vm_writer.writeFunction(func_name, str(nLocals))
                             self.compileSubroutineBody()
 
+
     def compileParameterList(self):
         if self.Tokens.keyWord() in ["INT", "CHAR", "BOOLEAN"] or self.Tokens.tokenType() == "IDENTIFIER":
             var_kind = self.SEGMENT_MAP["arg"]   # variable kind
@@ -136,6 +140,7 @@ class CompilationEngine:
                     self.symbol_table.define(name=var_name, type=var_type, kind=var_kind)
                     self.Tokens.advance()
 
+
     def compileSubroutineBody(self):
         if self.Tokens.symbol() == "{":
             self.Tokens.advance()
@@ -145,6 +150,7 @@ class CompilationEngine:
                 self.compileStatements()
             if self.Tokens.symbol() == "}":
                 self.Tokens.advance()
+
 
     def compileVarDec(self):
         if self.Tokens.keyWord() == "VAR":
@@ -168,6 +174,7 @@ class CompilationEngine:
                 if self.Tokens.symbol() == ";":
                     self.Tokens.advance()
 
+
     def compileStatements(self):
         while self.Tokens.keyWord() in ["LET", "DO", "IF", "WHILE", "RETURN"]:
             if self.Tokens.keyWord() == "LET":
@@ -181,9 +188,7 @@ class CompilationEngine:
             elif self.Tokens.keyWord() == "RETURN":
                 self.compileReturn()
 
-    ###########################
-    #### Fix let statement ####
-    ###########################
+
     def compileLet(self):
         array_flag = False
         if self.Tokens.keyWord() == "LET":
@@ -192,12 +197,12 @@ class CompilationEngine:
                 name = self.Tokens.getToken()
                 segment = self.symbol_table.KindOf(name)
                 index = self.symbol_table.IndexOf(name)
-                self.vm_writer.writePush(segment, str(index))
                 self.Tokens.advance()
                 if self.Tokens.symbol() == "[":
                     self.Tokens.advance()
                     if self.Tokens.symbol() != "]":
                         self.compileExpression()
+                        self.vm_writer.writePush(segment, str(index))
                         if self.Tokens.symbol() == "]":
                             self.vm_writer.writeArithmatic("add")
                             array_flag = True
@@ -207,18 +212,16 @@ class CompilationEngine:
                     if self.Tokens.symbol() != ";":
                         self.compileExpression()
                         if array_flag:
-                            self.vm_writer.writePop("pointer", "1")
-                            self.vm_writer.writePush("that", "0")
                             self.vm_writer.writePop("temp", "0")
                             self.vm_writer.writePop("pointer", "1")
                             self.vm_writer.writePush("temp", "0")
                             self.vm_writer.writePop("that", "0")
+                        else:
+                            self.vm_writer.writePop(segment, str(index))
                         if self.Tokens.symbol() == ";":
                             self.Tokens.advance()
 
-    ###########################
-    #### Fix do statement #####
-    ###########################
+
     def compileDo(self):
         if self.Tokens.keyWord() == "DO":
             self.Tokens.advance()
@@ -246,6 +249,7 @@ class CompilationEngine:
                     self.vm_writer.writeCall(name, str(nArgs))
                     self.vm_writer.writePop("temp", "0")
 
+
     def compileIf(self):
         labels = []
         if self.Tokens.keyWord() == "IF":
@@ -254,12 +258,12 @@ class CompilationEngine:
                 self.Tokens.advance()
                 if self.Tokens.symbol() != ")":
                     self.compileExpression()
+                    self.vm_writer.writeArithmatic("not")
                     if self.Tokens.symbol() == ")":
                         self.Tokens.advance()
                         if self.Tokens.symbol() == "{":
                             self.Tokens.advance()
                             if self.Tokens.symbol() != "}":
-                                self.vm_writer.writeArithmatic("not")
                                 labels.append(self.generateLabel())
                                 self.vm_writer.writeIf(labels[-1])
                                 self.compileStatements()
@@ -278,9 +282,7 @@ class CompilationEngine:
                                                 self.Tokens.advance()
                                 self.vm_writer.writeLabel(labels.pop(0))
 
-    ###########################
-    ### Fix while statement ###
-    ###########################
+
     def compileWhile(self):
         labels = []
         if self.Tokens.keyWord() == "WHILE":
@@ -291,12 +293,12 @@ class CompilationEngine:
                     labels.append(self.generateLabel())
                     self.vm_writer.writeLabel(labels[-1])
                     self.compileExpression()
+                    self.vm_writer.writeArithmatic("not")
                     if self.Tokens.symbol() == ")":
                         self.Tokens.advance()
                         if self.Tokens.symbol() == "{":
                             self.Tokens.advance()
                             if self.Tokens.symbol() != "}":
-                                self.vm_writer.writeArithmatic("not")
                                 labels.append(self.generateLabel())
                                 self.vm_writer.writeIf(labels[-1])
                                 self.compileStatements()
@@ -304,6 +306,7 @@ class CompilationEngine:
                                 if self.Tokens.symbol() == "}":
                                     self.Tokens.advance()
                                     self.vm_writer.writeLabel(labels.pop(0))
+
 
     def compileReturn(self):
         if self.Tokens.keyWord() == "RETURN":
@@ -315,6 +318,7 @@ class CompilationEngine:
             if self.Tokens.symbol() == ";":
                 self.vm_writer.writeReturn()
                 self.Tokens.advance()
+
 
     def compileExpression(self):
         if self.Tokens.getToken() not in CompilationEngine.SET:
@@ -334,9 +338,6 @@ class CompilationEngine:
                             self.vm_writer.writeCall(command, "2")
 
 
-    ###########################
-    #### Fix term statement ###
-    ###########################
     def compileTerm(self):
         if self.Tokens.getToken() not in CompilationEngine.SET:
             if self.Tokens.tokenType() == "INT_CONST":
@@ -358,7 +359,7 @@ class CompilationEngine:
                 elif self.Tokens.getToken() in ["false", "null"]:
                     self.vm_writer.writePush("constant", "0")
                 elif self.Tokens.getToken() == "true":
-                    self.vm_writer.writePush("constant", "0")
+                    self.vm_writer.writePush("constant", "1")
                     self.vm_writer.writeArithmatic("neg")
                 self.Tokens.advance()
             elif self.Tokens.symbol() in CompilationEngine.UNARY_OP:
@@ -377,12 +378,14 @@ class CompilationEngine:
                 if self.Tokens.symbol() == "[":
                     segment = self.symbol_table.KindOf(name)
                     index = self.symbol_table.IndexOf(name)
-                    self.vm_writer.writePush(segment, str(index))
                     self.Tokens.advance()
                     if self.Tokens.getToken() not in CompilationEngine.SET:
                         self.compileExpression()
+                        self.vm_writer.writePush(segment, str(index))
                         if self.Tokens.symbol() == "]":
                             self.vm_writer.writeArithmatic("add")
+                            self.vm_writer.writePop("pointer", "1")
+                            self.vm_writer.writePush("that", "0")
                             self.Tokens.advance()
                 elif self.Tokens.symbol() == "(":
                     self.Tokens.advance()
@@ -412,6 +415,7 @@ class CompilationEngine:
                     if self.Tokens.symbol() == ")":
                         self.Tokens.advance()
 
+
     def compileExpressionList(self):
         count = 0
         if self.Tokens.getToken() not in CompilationEngine.SET and self.Tokens.getToken() != ")":
@@ -424,9 +428,11 @@ class CompilationEngine:
                     count += 1
         return count
 
+
     def generateLabel(self):
         self.label += 1
         return "L" + str(self.label)
+
 
 if __name__ == "__main__":
 
